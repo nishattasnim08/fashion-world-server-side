9d0a17bb7c84b19e8a38dbc07b5d49dc5a27d133
nishattasnim08 <nishattasnim24258@gmail.com>
2022-05-09 17:36:43 +0600

    item deleted

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require("express");
 const app = express();
 var cors = require('cors')
@@ -15,19 +15,50 @@ const port = process.env.PORT || 5000;
 
 const uri = "mongodb+srv://fashiondbadmin:6y9Y6mEtWELpDwF7@cluster0.b0l5u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    console.log('CONNECTED');
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run() {
+    try {
+        await client.connect();
+        const dressCollection = client.db("fashiondb").collection("dresses");
+        // query for dresses that have a runtime less than 15 minutes
+        app.get("/dresses", async(req, res) => {
+
+            const query = {};
+
+            const cursor = dressCollection.find(query);
+            // replace console.dir with your callback to access individual elements
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
+        app.post("/dresses", async (req, res) => {
+            const newDress  = req.body
+            const result = await dressCollection.insertOne(newDress);
+            res.send(result);
+          });
+      
+          app.delete("/dress/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await dressCollection.deleteOne(query);
+            res.send(result)
+            
+          });
+      
+
+    } finally {
+        //   await client.close();
+    }
+}
+run().catch(console.dir);
 
 
 
 
 app.get("/", (req, res) => {
-  res.send("Server is running");
+    res.send("Server is running");
 });
 
 app.listen(port, () => {
-  console.log("Port is ", port);
+    console.log("Port is ", port);
 });
