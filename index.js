fe7d858295883316d3abcc51e3c0410c45631c1f
nishattasnim08 <nishattasnim24258@gmail.com>
2022-05-09 16:05:39 +0600

    database connected

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require("express");
 const app = express();
 var cors = require('cors')
@@ -7,6 +8,21 @@ app.use(express.json())
 
 
 const port = process.env.PORT || 5000;
+
+
+
+
+
+const uri = "mongodb+srv://fashiondbadmin:6y9Y6mEtWELpDwF7@cluster0.b0l5u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    console.log('CONNECTED');
+  // perform actions on the collection object
+  client.close();
+});
+
+
+
 
 app.get("/", (req, res) => {
   res.send("Server is running");
